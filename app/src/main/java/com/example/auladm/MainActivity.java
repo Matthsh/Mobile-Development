85a4b1fa9851f3475da428bf035f41047cd517e1
matt <mataus94@outlook.com>
2023-03-17 17:04:44 -0300

    1 parte feita

--- app/src/main/java/com/example/auladm/MainActivity.java
+++ app/src/main/java/com/example/auladm/MainActivity.java
@@ -3,40 +3,91 @@ package com.example.auladm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.auladm.models.User;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity implements Response.Listener<JSONArray>, Response.ErrorListener {
+    private EditText edit;
+    private List<User> users = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edit = findViewById(R.id.name);
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, "http://jsonplaceholder.typicode.com/users/2", null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
+                        User user = null;
+                        try {
+                            user = new User(
+                                    response.getInt("id"),
+                                    response.getString("name"),
+                                    response.getString("username"),
+                                    response.getString("email")
+                            );
+                            edit.setText(user.getName());
+                        } catch (JSONException e) {
+                            throw new RuntimeException("Erro no JSON: " + e);
+                        }
                         System.out.println("Chegou");
-                        System.out.println(response);
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
 
                     }
-                });
+                }, this);
         RequestQueue queue = Volley.newRequestQueue(this);
-        System.out.println("antes do add");
         queue.add(request);
-        System.out.println("depois do add");
-        System.out.println("ainda depois do add");
+        Button btn = findViewById(R.id.btBuscaTodos);
+        btn.setOnClickListener(view ->  {
+            getAllUsers();
+        });
+    }
+    private void getAllUsers() {
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, "http://jsonplaceholder.typicode.com/users", null,
+                this, this);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
+    }
+
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Toast.makeText(this, "Ocorreu uma falha na requisição" + error.getMessage(), Toast.LENGTH_LONG);
+    }
+
+    @Override
+    public void onResponse(JSONArray response) {
+        for (int i = 0; i < response.length(); i++) {
+            User user = null;
+            try {
+                JSONObject json = response.getJSONObject(i);
+                user = new User(
+                        json.getInt("id"),
+                        json.getString("name"),
+                        json.getString("username"),
+                        json.getString("email")
+                );
+                users.add(user);
+            } catch (JSONException e) {
+                throw new RuntimeException("Erro no JSON: " + e);
+            }
+        }
+        System.out.println(users);
     }
 }
--- app/src/main/java/com/example/auladm/models/User.java
+++ app/src/main/java/com/example/auladm/models/User.java
@@ -1,5 +1,55 @@
 package com.example.auladm.models;
 
-public class User {
+import androidx.annotation.NonNull;
 
+public class User {
+    private int id;
+    private String name;
+    private String user;
+    private String email;
+
+    public User(int id, String name, String user, String email) {
+        this.id = id;
+        this.name = name;
+        this.user = user;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "Nome: " + name + "; id: " + id +"\n";
+    }
 }
